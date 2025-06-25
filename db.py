f9ea1883f9f695f6596d5fa41b9360648ca546a3
unknown <fredrik.bramell@gmail.com>
2025-06-25 15:16:30 +0200

    More rigorous valdiation and search function added

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import json
+import re
 
 # Local/URI connection
 client = MongoClient("mongodb://localhost:27017")
@@ -10,46 +11,61 @@ collection = db["books"]
 
 
 
-# Takes dict {title, author, isbn, year, tags}, 
+''' Takes dict {title, author, isbn, year, tags} '''
 def add_book(book_data): 
     # Validate data --> add book
     if book_data != {}:
         collection.insert_one(book_data)
 
 
-# Returns a list of books (Note: ObjectId is not filtered out)
+
+''' Returns a list of books ''' 
+# (Note: ObjectId is not filtered out)
 def get_all_books():
     return list(collection.find())
 
 
 
+''' Function for deleting all database entries '''
+def delete_all_docs():
+    collection.delete_many({})
+
+    # Check if deletion was successful by retrieving and counting the collection from get_all_books()
+    return "\n All entries deleted \n" if len(get_all_books()) <= 0 else "\n Could not remove any books \n" # Practicing ternary operators
+
+
+
+''' Function for deleting one book '''
 def delete_book(query):
-    # Count the amount of books before and after removal 
+    #Get the title of the book - only for confirmation message to user (instead of getting the ISBN nr)
+    title = search_books(query)
+
+    # Count the amount of books before and after removal
     count_before = get_all_books()
-    collection.delete_one({"title": query})
+    collection.delete_one({"isbn": query})
     count_after = get_all_books()
 
     # Comparison to see if the book is removed
     if len(count_before) > len(count_after):
-        return "The book {title} was successfully removed".format(title=query)
+        return "-- The book {title} was successfully removed --".format(title=title["title"]) # title["titel"] = the return value from search_books()
     else: 
-        return "The book could not be removed, did you spell it correctly?"
-    
-
-
-def delete_all_docs():
-    collection.delete_many({})
-    print("¤¤¤¤¤", collection, "¤¤¤¤¤")
-    collection.find
+        return "-- The book could not be removed, did you enter the correct ISBN-number (lower back of the book)? --" # Too long for ternary op, harder to read
 
 
 
-'''
+''' Find several books by author or one by ISBN '''
 def search_books(query):
-    1. Check if query is a str or int
-    1. If str --> Use author to display all their books --> collection.find(query) --> return list
-    2. If int --> Use ISBN number for unique search --> collection.find_one(query) --> return Author, Title, Year, Tag
-'''
+    #If query == int (ISBN) --> display one book (ISBN = unique)
+    if all(char.isdigit() or char == '-' for char in query):
+        return collection.find_one({"isbn": query})
+
+    # Search for all books by the author (case-sensitive)
+    else:
+        # $regex: f"^{query}$" searches for the exakt match
+        return list(collection.find({"author": {"$regex": f"^{query}$", "$options": "i"}})) #   ^ = beginning of string, $ = end of string
+
+        
+
 
 
 '''
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import re
 from db import *
 
+
+''' Interface menu to navigate the program. '''
 def menu():
     while True:
         print("\n", "---------- MENU ----------")
         print("1. Add Book")
         print("2. Delete Book")
         print("3. View Collection")
+        print("4. Search books")
         print("9. Delete All Books")
         print("0. Quit")
         choice = int(input("Choose an option: "))
@@ -16,53 +20,115 @@ def menu():
             delete_book_prompt()
         elif choice == 3:
             view_collection()
+        elif choice == 4:
+            search_books_prompt()
         elif choice == 9:
             delete_all_docs_prompt()
         else:
             break
 
+        print("")
 
 
+
+''' Ask the user for inputs, validate and send to database. '''
 def add_book_prompt():
-    title = input("Title: ")
-    author = input("Author: ")
-    isbn = input("ISBN number: ")
-    year = input("Year: ")
-    tags = input("Tag/Category: ")
+    print("\n", "---------- ADD A BOOK ----------", "\n")
+    validated = False
 
-    book_data = {
-        "title" : title,
-        "author" : author,
-        "isbn" : isbn,
-        "year" : year,
-        "tags" : tags
-    }
+    while not validated:
+        # Titles can be any characters or numbers so no validation here.
+        title = input("Title: ")
+
+        # Using re to allow special characters but prevent empty strings or @123 characters
+        author = input("Author: ")
+        if not re.fullmatch(r"[A-Za-zÀ-ÖØ-öø-ÿ\- ]+", author): # Syntax: re.fullmatch(pattern, string)
+            print("Author name can only contain letters, hyphens, and spaces.")
+        
+        # Using whitelist to check for illegal characters, can't use isdigit due to '-' char
+        # ¤¤¤   Add format checking     ¤¤¤
+        whitelisted_chars = "0123456789-"
+        isbn = input("ISBN number: ")
+        if any(char not in whitelisted_chars for char in isbn):
+            print("ISBN must be numbers and in format: xxx-xx-x-xxxxxx-x (- is allowed)")
+            continue
+        
+        # Using isdigit to check that input is an integer
+        year = input("Year: ")
+        if year.isdigit() == False:
+            print("Year needs to be a number")
+            continue
+        
+        # Tags could technically be anything, but only categories in string format are preferred.
+        tags = input("Tag/Category: ")
+        if not re.fullmatch(r"[A-Za-zÀ-ÖØ-öø-ÿ\- ]+", author):
+            print("A tag has to be text, not numbers or special characters")
+        
+        # If all data is ok, set validate to True
+        validated = True
+        if validated:
+            book_data = {
+            "title" : title,
+            "author" : author,
+            "isbn" : isbn,
+            "year" : year,
+            "tags" : tags
+        }
+        
+        # Ensure that book_data isn't an empty dict --> send dict to db.py
+        if book_data != {}:
+            add_book(book_data)
     
-    if book_data != {}:
-        add_book(book_data)
 
 
-
+''' Function for printing the collection '''
 def view_collection():
     print("\n", "---------- VIEW COLLECTION ----------", "\n")
     books = get_all_books()
     for book in books:
-        print("Title: ", book["title"], "  ||  ", "Author: ", book["author"])
-        print("-"*37)
+        print("Title: ", book["title"], "  ||  ", "Author: ", book["author"], "  ||  ", "ISBN: ", book["isbn"])
+        print("-"*100)
 
 
 
+''' Function that takes an input to send to db.py for deletion '''
 def delete_book_prompt():
-    title = str(input("\n", "Title of the book you want to delete: "))
-    print("\n", delete_book(title))
+    print("")
+    print("\n", "---------- DELETE A BOOK ----------", "\n")
+    # Does not have to be ISBN, but since it's unique it's good for removing the right book if duplicates with different ISBN numbers have been entered
+    # If any other more common variable like "year" is used, the first found entry/document will be deleted from the db
+    
+    isbn = input("ISBN number of the book you want to delete: ") 
+
+    # Using whitelist to check for illegal characters, can't use isdigit due to '-' char
+    whitelisted_chars = "0123456789-"
+    if any(char not in whitelisted_chars for char in isbn):
+        print("ISBN must be numbers and in format: xxx-xx-x-xxxxxx-x (- is allowed)")
+
+    # Execute the function and print the return statement
+    else:
+        print("\n", delete_book(isbn))
 
 
 
+''' Function to delete all entries to the database '''
 def delete_all_docs_prompt():
-    confirmation = input("Are you sure (y/n)? ")
+    print("\n", "---------- DELETE ALL BOOKS ----------", "\n")
+    confirmation = input("NOTE: This action can't be undone. Are you sure (y/n)? ")
     if confirmation == "y":
-        delete_all_docs()
+        # Execute the function and print the return statement
+        print(delete_all_docs()) 
     else:
         pass
+
+
+
+''' Function to search for all books by author or one by ISBN '''
+def search_books_prompt():
+    print("\n", "---------- SEARCH BOOKS ----------", "\n")
+    query = input("Enter the name of an author for all their books or an ISBN number for one book (with format: xxx-xx-x-xxxxxx-x): ")
+    
+    print(search_books(query))
+
 
 menu()
